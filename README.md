bff3aa0a7cba831cb740e52e38004a38b73ab0b6
Bharat Sharma <bharat.sharma1809@gmail.com>
2021-04-18 02:54:43 +0530

    ptch: update and add comments

--- README.md
+++ README.md
@@ -1,5 +1,69 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# License Detector (Experimental)
 
-Created from templates made available by Stagehand under a BSD-style
-[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
+## Current Implementation:
+
+- Normalization of text:
+    - Copyright Notice
+    - License Header
+    - Bullets
+    - Extraneous White Spaces
+- Tokenization and generation of hashed q-grams. The Crc32 algorithm is used for hashing the q-grams.
+- Check whether a possible match or not and then calculate the final confidence.
+
+*NOTE:* The current implementation is just for experimental purpose. Its just a depiction of how the application will work.
+
+<br>
+
+**After testing for various threshold values, I have found that a threshold of 85% works well.**
+
+**Explanation:** In some cases, the Dice Coefficient was near above 85% however the final confidence was about 95%, hence the 85% threshold value fits for almost all licenses.
+
+[LicenseClassifier](https://github.com/google/licenseclassifier/blob/bb04aff29e72e636ba260ec61150c6e15f111d7e/stringclassifier/classifier.go#L70) which uses almost the same approach to detect licenses has a [threshold value](https://github.com/google/licenseclassifier/blob/bb04aff29e72e636ba260ec61150c6e15f111d7e/stringclassifier/classifier.go#L70) of 80% 
+
+<br>
+
+### Output for a normal case:
+```
+Normalizing texts...
+Normalized --> Elapsed: 21 ms
+
+Tokenizing...
+Created tokens --> Elapsed: 0 ms
+
+Creating q-grams...
+Created q-grams --> Elapsed: 15 ms
+
+Calculating possibility...
+It is a possible match: 94.32098765432099% --> Elapsed: 33 ms
+
+Calculating final confidence...
+Calculated confidence --> Elapsed: 16 ms
+
+Confidence: 98.44632768361582%
+
+Its a match!!
+```
+<br>
+
+### Output when the order of the text is changed:
+```
+Normalizing texts...
+Normalized --> Elapsed: 20 ms
+
+Tokenizing...
+Created tokens --> Elapsed: 0 ms
+
+Creating q-grams...
+Created q-grams --> Elapsed: 12 ms
+
+Calculating possibility...
+It is a possible match: 88.39506172839506% --> Elapsed: 23 ms
+
+Calculating final confidence...
+Calculated confidence --> Elapsed: 30 ms
+
+Confidence: 73.94067796610169%
+
+Not a match :(
+``` 
+**The above example clearly demonstrates why the Dice Coefficient is not a determining factor because it doesn't take into account the order of the text. The unordered text passes the possibility check, however, its not a match since the order of the text is different. Calculating the confidence using the Levenshtein distance solves the problem.**
--- bin/spdx_lic_detector.dart
+++ bin/spdx_lic_detector.dart
@@ -4,46 +4,11 @@ import 'dart:math';
 import 'package:dmp/dmp.dart';
 import 'package:archive/archive_io.dart';
 
-void main(List<String> arguments) {
-  final s1 = '''Copyright 2014 The Flutter Authors. All rights reserved.
+import 'texts.dart';
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of Google Inc. nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-''';
-
-  final s2 = '''Copyright (c) <year> <owner>. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-''';
+void main() {
+  final s1 = unknown_with_changed_seq;
+  final s2 = known;
 
   final stpWatch = Stopwatch();
 
@@ -68,19 +33,28 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
   print('Calculating possibility...');
   final diceCoeff = diceCoefficent(qGram1, qGram2);
   if (diceCoeff < threshold) {
-    print('Not a possible match since dice coefficient ($diceCoeff) < $threshold');
+    print('Not a possible match since dice coefficient: $diceCoeff < $threshold');
     return;
   }
-  print('It is a possible match: ($diceCoeff) --> Elapsed: ${stpWatch.elapsedMilliseconds} ms\n');
+  print(
+      'It is a possible match: ${diceCoeff * 100}% --> Elapsed: ${stpWatch.elapsedMilliseconds} ms\n');
   stpWatch.reset();
 
   print('Calculating final confidence...');
   final confidence = finalConfidence(norm1, norm2);
   print('Calculated confidence --> Elapsed: ${stpWatch.elapsedMilliseconds} ms\n');
   stpWatch.stop();
-  print('Confidence: $confidence%\n\n');
+  print('Confidence: ${confidence * 100}%\n');
+
+  if (confidence > threshold) {
+    print('Its a match!!\n');
+  } else {
+    print('Not a match :(\n');
+  }
 }
 
+/// Normalize the text
+/// Currently handles header, copyright notice, list indicators/bullets, spaces
 String normalize(String s) {
   final header = RegExp(r'[\s\S]*?(?=Copyright)');
   final copyrightText = RegExp(r'(^Copyright).*', multiLine: true);
@@ -95,11 +69,13 @@ String normalize(String s) {
   return s;
 }
 
+/// Tokenize the text
 List<String> tokenize(String s) {
   final str = s.split(' ');
   return str;
 }
 
+/// Create hashed q-grams of the text
 List createQGrams(List<String> tokens) {
   final q = max(1, (threshold / (1 - threshold))).floor();
   var qGrams = [];
@@ -118,6 +94,8 @@ List createQGrams(List<String> tokens) {
   return qGrams;
 }
 
+/// Calculate the Sorenson dice coefficient
+/// Its a measure to determine whether it can be a possible match or not
 double diceCoefficent(List unknown, List known) {
   final set1 = unknown.toSet();
   final set2 = known.toSet();
@@ -128,11 +106,12 @@ double diceCoefficent(List unknown, List known) {
   return diceCoeff;
 }
 
+/// Calculate the final confidence using the Levenshtein distance
 double finalConfidence(String s1, String s2) {
   final dmp = DiffMatchPatch();
   final diffs = dmp.diff_main(s1, s2);
   final lev = dmp.diff_levenshtein(diffs);
-  final confidence = (1 - (lev / s2.length)) * 100;
+  final confidence = 1 - (lev / s2.length);
   return confidence;
 }
 
